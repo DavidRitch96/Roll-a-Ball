5cd1ebcd20d3efa42b98454fc74de043584a2c0e
DavidRitch96 <david.ritch@mail.mcgill.ca>
2017-10-14 17:21:27 -0400

    Commit

--- Assets/Scripts/Restarter.cs
+++ Assets/Scripts/Restarter.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Restarter : MonoBehaviour {
 
+
 	// Update is called once per frame
 	void Update () {
+
 
 		if( Input.GetKeyDown(KeyCode.R) )
 		{
